8331a850bb2994fcaa7e053cb6c526cda08232ce
marssilen <missa@chmail.ir>
2017-02-12 01:20:39 +0330

    2

--- app/core/ControllerPanel.php
+++ app/core/ControllerPanel.php
@@ -1,34 +1,26 @@
 <?php
-class ControllerPanel extends Controller 
+class ControllerPanel extends Controller
 {
 	function __construct(){
 		Session::init();
 		$logged=Session::get('loggedIn');
 		if($logged==false || Session::timeout()){
 			Session::destroy();
-			header('location: ../login');
+			header('location: '.URL.'login');
 			exit;
 		}
 	}
-	/*public function model ($model){
-		require_once '../app/models/'.$model.'.php';
-		return new $model();
-	}*/
+
 	public function view($view,$data=[],$secure=false){
 		if($secure){
 			$logged=Session::get('role');
 			if($logged!='admin'){
-				/*Session::destroy();
-				header('location: ../login');
-				exit;*/
-				
-			}else{
-				require_once 'app/views/'.$view.'.php';
+				Session::destroy();
+				header('location: '.URL.'login');
+				exit;
 			}
-			
-		}else{
-			require_once 'app/views/'.$view.'.php';
+			//
 		}
-		
+		require_once 'app/views/'.$view.'.php';
 	}
 }
